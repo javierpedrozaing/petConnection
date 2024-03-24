38ff2d612252182db2971c9b581461428092981f
Javier Pedroza <jpedroza@spreedly.com>
2024-03-24 18:07:09 -0500

    Add user entity

---
+++ petConnection/petConnection.Share/Entitties/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace petConnection.Share.Entitties
+{
+	public class User
+	{
+		public int Id { get; set; }
+
+        [MaxLength(255)]
+        public string Role { get; set; }
+
+		[MaxLength(255)]
+		public string UserName {get; set;}
+
+		[Required]
+		public string Email { get; set; }
+
+
+		public string Password { get; set; }
+	}
+	
+}
+
