c97c9231ffb468c9594b0d78ac40472313fb9ce5
Javier Pedroza <jpedroza@spreedly.com>
2024-03-24 17:55:38 -0500

    upload basic code

---
+++ petConnection/petConnection.Share/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace petConnection.Share;
+
+public class Class1
+{
+
+}
+
---
+++ petConnection/petConnection.UnitTest/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace petConnection.UnitTest;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
